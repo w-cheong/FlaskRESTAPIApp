45d90342cbbbbd1052f6c3f2f164e83e941b380f
Wilson <wilsoncheong7405@gmail.com>
2025-11-05 17:47:29 -0500

    done listing with pagination

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+Routes 
+
+* [X] GET `/characters` -- list all characters with pagination
+* [ ] GET `/characters/search` -- search for a character by first or last name
+* [ ] PUT `/characters/<id>` -- update a character by id and persist to CSV
+* [ ] DELETE `/characters/<id>` -- remove a character by id and persist to CSV
+
+
+Examples:
+* `GET /characters/search?first_name=Phoebe`
+* `GET /characters/search?last_name=Geller`
+* `GET /characters?page=1&per_page=10`
+
--- app.py
+++ app.py
@@ -5,14 +5,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# reading the csv from raw url into dataframe
-url = 'https://raw.githubusercontent.com/gchandra10/filestorage/refs/heads/main/friends_data.csv'
-# friends_data = 'friends_data.csv'
-df = pd.read_csv(url, index_col=0)
-page_At = '&_page='
-page_Total = '&_per_page='
-per_page = 10
-page = 1
+FILE_PATH = 'friends_data.csv'
+df = pd.read_csv(FILE_PATH, index_col=0)
 
 # home page 
 @app.route('/', methods=["GET"])
@@ -21,26 +15,37 @@ def home_page():
 
 # pagination
 @app.route('/characters', methods=["GET"])
+def list_all_characters():
+    print(request.args)
 
-def paginate_data(df, page, page_size=10):
-    data_found = []
-    
-    current_page = request.args.get('characters')
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per-page", 10, type=int)
 
-    # for page in range(page_size):
-    #     page_url = url + page_At + str(page) + page_Total +     
-    #     pages = df.paginate(per_page, page_size).items      
-    #     return jsonify(page)
+    # todo error handling, for invalid page numbers
 
-# searching by first/last name
+    start_index = per_page * (page - 1)
+    end_index = per_page * (page)
+    results = df.iloc[start_index:end_index]
+    print(results)
+
+    # convert dataframe into list of dictionaries for jsoinifying
+    return jsonify(results.to_dict(orient='records'))
+
+# TODO searching by first/last name
 @app.route('/characters/search', methods=["GET"])
 def search_items():
     query_string = request.query_string
     return f'Query String: {query_string}'
 
-# update character by id
-@app.route('/characters', methods=["PUT"])
-def update_character(): 
+# TODO update character by id
+@app.route('/characters/<int:id>', methods=["PUT"])
+def update_character(id):
+    # TODO
+    return jsonify()
+
+@app.route('/characters/<int:id>', methods=["DELETE"])
+def delete_character(id):
+    # TODO
     return jsonify()
 
 if __name__ == '__main__':
