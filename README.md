133422ca5852db601887f94008a6297a9c68e5f4
Wilson <wilsoncheong7405@gmail.com>
2025-11-06 03:18:46 -0500

    working route to delete character by id

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
+FlaskRestAPIApp
 
+This program reads data from a csv using pandas built as a Flask REST API application.
 
 Routes 
 
 * [X] GET `/characters` -- list all characters with pagination
-* [ ] GET `/characters/search` -- search for a character by first or last name
-* [ ] PUT `/characters/<id>` -- update a character by id and persist to CSV
-* [ ] DELETE `/characters/<id>` -- remove a character by id and persist to CSV
+* [X] GET `/characters/search` -- search for a character by first or last name
+* [X] PUT `/characters/<id>` -- update a character by id and persist to CSV
+* [X] DELETE `/characters/<id>` -- remove a character by id and persist to CSV
 
 
 Examples:
+* `GET /characters?page=1&per_page=10`
 * `GET /characters/search?first_name=Phoebe`
 * `GET /characters/search?last_name=Geller`
-* `GET /characters?page=1&per_page=10`
-
+* `PUT /characters/1`
+* `DELETE /characters?1`
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify, request
 import csv
-# from flask_restful import Resource, Api, reqparse
-# from flask_sqlalchemy import pagination
 import pandas as pd
 
 app = Flask(__name__)
@@ -14,24 +12,24 @@ df = pd.read_csv(FILE_PATH, index_col=0)
 def home_page():
     return jsonify("Page here")
 
-# pagination
+# list all characters with pagination
 @app.route('/characters', methods=["GET"])
 def list_all_characters():
-    print(request.args)
+    # print(request.args)
 
     page = request.args.get("page", 1, type=int)
     per_page = request.args.get("per-page", 10, type=int)
 
-    # TODO: error handling, for invalid page numbers
+    # calculates for correct page numbers
     start_index = per_page * (page - 1)
     end_index = per_page * (page)
     results = df.iloc[start_index:end_index]
-    print(results)
+    # print(results)
 
     # convert dataframe into list of dictionaries for jsonifying
     return jsonify(results.to_dict(orient='records'))
 
-# searching by first/last name
+# search by first/last name
 @app.route('/characters/search', methods=["GET"])
 def search_items():
     first_names = request.args.get('first_name') # query parameter setup
@@ -45,9 +43,9 @@ def search_items():
         search_results = df[df['last_name'] == last_names]
         return jsonify(search_results.to_dict(orient='records'))
     else: #error handling if not searching for name properly
-        return jsonify({"message": "names cannot be found."}), 404
+        return jsonify({"message": "Names cannot be found."}), 404
 
-# TODO: update character by id
+# update character by id
 @app.route('/characters/<int:id>', methods=["PUT"])
 def update_character(id):
     character_data = request.get_json()
@@ -55,12 +53,16 @@ def update_character(id):
         df.loc[id] = character_data #access rows for changing
         df.to_csv(FILE_PATH, quoting=csv.QUOTE_ALL)
         return jsonify(character_data)
-    return jsonify({"message": "character cannot be found."}), 404
+    return jsonify({"message": "Character cannot be found."}), 404
 
+# delete character by id
 @app.route('/characters/<int:id>', methods=["DELETE"])
 def delete_character(id):
-    # TODO:
-    return jsonify()
+    if id: #check if id is found in the csv
+        df.drop(id, inplace=True) #drops the row inplace with the given id
+        df.to_csv(FILE_PATH, quoting=csv.QUOTE_ALL)
+        return jsonify({"message": "Character deleted."})
+    return jsonify({"message": "Character cannot be found."}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
